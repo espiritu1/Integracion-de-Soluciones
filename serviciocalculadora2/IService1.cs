746808398be3142edc1c1fb9f124ba0c944af036
espiritu1 <37638028+espiritu1@users.noreply.github.com>
2021-09-15 21:57:53 -0500

    8
    8

---
+++ serviciocalculadora2/IService1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace serviciocalculadora2
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        int SumaDosEnteros(int valor1, int valor2);// suma
+        [OperationContract]
+        int RestaDosEnteros(int valor1, int valor2);//resta
+        [OperationContract]
+        int MultiplicaDosEnteros(int valor1, int valor2);//multiplicacion
+        [OperationContract]
+         double DivideDosEnteros(double valor1, double valor2);//divicion
+
+
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ serviciocalculadora2/Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace serviciocalculadora2
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        public int SumaDosEnteros(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public int RestaDosEnteros(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+
+        public int MultiplicaDosEnteros(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public double DivideDosEnteros(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
